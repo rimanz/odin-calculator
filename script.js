5b3ad18bce25e2e366bfecee10a9b8188a8879cc
Riman Das <34216663+rimanz@users.noreply.github.com>
2024-12-07 20:47:52 +0000

    Add `operate` method to the `calculator` object.
    The operate method apply diffrent arithmetic operations on the operands
    depending on diffrent operator value.

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const calculator = new function() {
     this.a = null;  // first operand
     this.b = null;  // second operand
-    this.operator = null;
+    this.operator = null;   // operator
 
     // Arithmetic methods:
     this.add = () => this.a + this.b;
     this.subtract = () => this.a - this.b;
     this.multiply = () => this.a * this.b;
     this.divide = () => (this.a / this.b).toFixed(2);
+
+    // Operational methods:
+    this.operate = () => {
+        switch (this.operator) {
+            case '+':
+                return this.add();
+            case '−':
+                return this.subtract();
+            case '×':
+                return this.multiply();
+            case '÷':
+                return this.divide();
+        }
+    }
 }
