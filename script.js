04e83e7095325d414b6ec5aeb986f1adab59cae8
Riman Das <34216663+rimanz@users.noreply.github.com>
2024-12-09 19:48:37 +0600

    Add event listener and a handler function
    The handler function is still in work-in-progress

--- script.js
+++ script.js
@@ -23,3 +23,41 @@ const calculator = new function() {
         }
     }
 }
+
+// DOM Elements and Event Listeners:
+const display = document.querySelector('.display');
+const keypad = document.querySelector('.keypad');
+
+keypad.addEventListener('click', handleClick);
+
+// Functions:
+function handleClick(event) {
+    const key = event.target;
+    const keyValue = key.textContent;
+    const keyType = key.getAttribute('data-key-type');
+    console.log(key, keyValue, keyType);
+
+    switch (keyType) {
+        case 'action':
+            // Perform some action according to the button.
+            break;
+        case 'number':
+            // Append Number to the display.
+            break;
+        case 'operator':
+            /*  if both operands exist:
+                    perform operation based on previous operator value.
+                    and display the value
+                    and update first operand with display value.
+                    update operator with keyValue
+                    clear second operand
+                else if only first operand exists:
+                    update operator with keyValue
+                    update second operand with display value
+                else if no operands defined:
+                    update first operand with display value
+                    update operator with keyValue
+            */
+            break;
+    }
+}
